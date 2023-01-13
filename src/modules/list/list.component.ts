36bcfe192f7e824a56d52149a858f67195cfc777
ababakhanyan <ababakhanyan@phoenixit.ru>
2023-01-13 16:03:19 +0300

    update

--- src/modules/list/list.component.ts
+++ src/modules/list/list.component.ts
@@ -1,11 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { IToDo, TodoService } from 'src/app/services/todo/todo.service';
 
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 
 export class ListComponent implements OnInit {
